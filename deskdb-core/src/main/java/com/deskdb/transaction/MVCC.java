ae6b36d68bdcc4e7cb92fd6abd683866adaf8e79
yasmramos <yasmramos95@gmail.com>
2026-07-28 15:40:17 +0000

    fix: add missing HashMap import in MVCC.java

--- deskdb-core/src/main/java/com/deskdb/transaction/MVCC.java
+++ deskdb-core/src/main/java/com/deskdb/transaction/MVCC.java
@@ -2,6 +2,7 @@ package com.deskdb.transaction;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
